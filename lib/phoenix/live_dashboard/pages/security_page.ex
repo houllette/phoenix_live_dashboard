bb08cd6c126689fe92f290776e7d3b68359d3d33
Holden Oullette <holden.oullette@podium.com>
2023-03-06 14:03:49 -0800

    Sobelow and MixAudit skeleton

--- lib/phoenix/live_dashboard/pages/security_page.ex
+++ lib/phoenix/live_dashboard/pages/security_page.ex
@@ -11,10 +11,72 @@ defmodule Phoenix.LiveDashboard.SecurityPage do
 
   @impl true
   def render_page(assigns) do
+    nav_bar(
+      items: [
+        sobelow: [
+          name: "Sobelow",
+          render: fn -> sobelow_nav_item(assigns) end
+        ],
+        mix_audit: [
+          name: "MixAudit",
+          render: fn -> mix_audit_nav_item(assigns) end
+        ]
+      ]
+    )
+  end
+
+  defp sobelow_nav_item(assigns) do
+    row(
+      components: [
+        columns(
+          components: [
+            sobelow_info_row(assigns)
+          ]
+        )
+      ]
+    )
+  end
+
+  defp sobelow_info_row(_assigns) do
+    row(
+      components: [
+        columns(
+          components: [
+            card(
+              title: "Sobelow Findings",
+              hint: "Findings counts are based on the defined confidence threshold",
+              inner_title: "Critical",
+              value: 0
+            ),
+            card(
+              inner_title: "High",
+              value: 0
+            )
+          ]
+        ),
+        columns(
+          components: [
+            card(
+              inner_title: "Medium",
+              value: 0
+            ),
+            card(
+              inner_title: "Low",
+              value: 0
+            ),
+            card(
+              inner_title: "Informational",
+              value: 0
+            )
+          ]
+        )
+      ]
+    )
+  end
+
+  defp mix_audit_nav_item(_assigns) do
     card(
-      title: "Sobelow",
-      inner_title: "Findings",
-      class: ["additional-class"],
+      inner_title: "Coming Soon",
       value: 0
     )
   end
